add797b9df019e1d12e53476461e71dc7ce541e4
robaita <robaita2021@gmail.com>
2022-07-03 21:46:30 +0530

    initial commit

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,206 +1,145 @@
 import React, { useEffect, useRef } from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
-import styled from 'styled-components';
-import { srConfig } from '@config';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
 import sr from '@utils/sr';
-import { usePrefersReducedMotion } from '@hooks';
+import { srConfig, github } from '@config';
+import styled from 'styled-components';
+import { theme, mixins, media, Section, Heading } from '@styles';
+const { colors, fontSizes, fonts } = theme;
 
-const StyledAboutSection = styled.section`
-  max-width: 900px;
-
-  .inner {
-    display: grid;
-    grid-template-columns: 3fr 2fr;
-    grid-gap: 50px;
-
-    @media (max-width: 768px) {
-      display: block;
-    }
+const StyledContainer = styled(Section)`
+  position: relative;
+`;
+const StyledFlexContainer = styled.div`
+  ${mixins.flexBetween};
+  align-items: flex-start;
+  ${media.tablet`display: block;`};
+`;
+const StyledContent = styled.div`
+  width: 60%;
+  max-width: 480px;
+  ${media.tablet`width: 100%;`};
+  a {
+    ${mixins.inlineLink};
   }
 `;
-const StyledText = styled.div`
-  ul.skills-list {
-    display: grid;
-    grid-template-columns: repeat(2, minmax(140px, 200px));
-    grid-gap: 0 10px;
-    padding: 0;
-    margin: 20px 0 0 0;
-    overflow: hidden;
-    list-style: none;
-
-    li {
-      position: relative;
-      margin-bottom: 10px;
-      padding-left: 20px;
-      font-family: var(--font-mono);
-      font-size: var(--fz-xs);
-
-      &:before {
-        content: '▹';
-        position: absolute;
-        left: 0;
-        color: var(--green);
-        font-size: var(--fz-sm);
-        line-height: 12px;
-      }
-    }
+const SkillsContainer = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(140px, 200px));
+  overflow: hidden;
+  padding: 0;
+  margin: 20px 0 0 0;
+  list-style: none;
+`;
+const Skill = styled.li`
+  position: relative;
+  margin-bottom: 10px;
+  padding-left: 20px;
+  font-family: ${fonts.SFMono};
+  font-size: ${fontSizes.smish};
+  color: ${colors.green};
+  &:before {
+    content: '▹';
+    position: absolute;
+    left: 0;
+    color: ${colors.green};
+    font-size: ${fontSizes.sm};
+    line-height: 12px;
   }
 `;
 const StyledPic = styled.div`
   position: relative;
+  width: 40%;
   max-width: 300px;
-
-  @media (max-width: 768px) {
-    margin: 50px auto 0;
-    width: 70%;
-  }
-
-  .wrapper {
-    ${({ theme }) => theme.mixins.boxShadow};
-    display: block;
-    position: relative;
-    width: 100%;
-    border-radius: var(--border-radius);
-    background-color: var(--green);
-
-    &:hover,
+  margin-left: 60px;
+  ${media.tablet`margin: 60px auto 0;`};
+  ${media.phablet`width: 70%;`};
+  a {
     &:focus {
       outline: 0;
-
-      &:after {
-        top: 15px;
-        left: 15px;
-      }
-
-      .img {
-        filter: none;
-        mix-blend-mode: normal;
-      }
-    }
-
-    .img {
-      position: relative;
-      border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
-      transition: var(--transition);
-    }
-
-    &:before,
-    &:after {
-      content: '';
-      display: block;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      border-radius: var(--border-radius);
-      transition: var(--transition);
-    }
-
-    &:before {
-      top: 0;
-      left: 0;
-      background-color: var(--navy);
-      mix-blend-mode: screen;
-    }
-
-    &:after {
-      border: 2px solid var(--green);
-      top: 20px;
-      left: 20px;
-      z-index: -1;
     }
   }
 `;
-
-const About = () => {
-  const revealContainer = useRef(null);
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
+const StyledAvatar = styled(Img)`
+  position: relative;
+  mix-blend-mode: multiply;
+  filter: grayscale(100%) contrast(1);
+  border-radius: ${theme.borderRadius};
+  transition: ${theme.transition};
+`;
+const StyledAvatarLink = styled.a`
+  ${mixins.boxShadow};
+  width: 100%;
+  position: relative;
+  border-radius: ${theme.borderRadius};
+  background-color: ${colors.lightestSlate};
+  margin-left: -20px;
+  &:hover,
+  &:focus {
+    background: transparent;
+    &:after {
+      top: 15px;
+      left: 15px;
     }
+    ${StyledAvatar} {
+      filter: none;
+      mix-blend-mode: normal;
+    }
+  }
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: ${theme.borderRadius};
+    transition: ${theme.transition};
+  }
+  &:before {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: ${colors.navy};
+    mix-blend-mode: screen;
+  }
+  &:after {
+    border: 2px solid ${colors.green};
+    top: 10px;
+    left: 10px;
+    z-index: -1;
+  }
+`;
 
-    sr.reveal(revealContainer.current, srConfig());
-  }, []);
-
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+const About = ({ data }) => {
+  const { frontmatter, html } = data[0].node;
+  const { title, skills, avatar } = frontmatter;
+  const revealContainer = useRef(null);
+  useEffect(() => sr.reveal(revealContainer.current, srConfig()), []);
 
   return (
-    <StyledAboutSection id="about" ref={revealContainer}>
-      <h2 className="numbered-heading">About Me</h2>
-
-      <div className="inner">
-        <StyledText>
-          <div>
-            <p>
-            Hi, I am Avinash Kumar Singh, currently I am a Post-Doctoral Research Fellow in Department of Computing Science, 
-            Umea University Sweden . We do work on training the robots in such a way that, if they will have any conflict with 
-            regard to performing any activity, they raise their concern and ask for the external help to resolve that conflict. 
-            We are using Pepper robot as our testbed to test our algorithms. Prior to join this Post-Doctoral Program, 
-            I was associated with HCL Technologies as an Deputy Manager. There, I headed the Machine Learning team to design 
-            various solutions, such as an intelligent KYC system, Information Mining, Signature Verification, Automatic Cheque Clarence. 
-            I had a chance to work with eClerx Services located at Navi Mumbai as a Technology Specialist for Machine Learning. 
-            I stayed there for almost one and half year and conducted various projects related to Email Classification, 
-            Anaphora Resolution, Document Colouring.
-            </p>
-
-            <p>
-            I am a Ph.D. in Human Robot Interaction from Robotics and Artificial Intelligence Laboratory, 
-            Indian Institute of Information Technology, Allahabad. During my Ph.D., we aimed to build a reliable and intelligent 
-            framework for robots, so that they could interact with human as we do in our daily life. We have developed a facial 
-            symmetry and component-based face recognition system to mitigate the variable illumination and expression scenarios. 
-            Since we have used only a part of the face, the approach is computationally faster. A research is also conducted to help 
-            police in criminal identification. We had NAO humanoid robot which was trained to establish the verbal communication with 
-            human in the natural language (English). Nao communicates with the eyewitness and tries to collect the visual as well 
-            physiological description about the criminal. This visual description is then processed to find the match with the 
-            existing database, if the match is found, the information about the criminal is retrieved otherwise Nao creates the 
-            sketch of the same.
-            </p>
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
-          </div>
-
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
-        </StyledText>
-
+    <StyledContainer id="about" ref={revealContainer}>
+      <Heading>{title}</Heading>
+      <StyledFlexContainer>
+        <StyledContent>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <SkillsContainer>
+            {skills && skills.map((skill, i) => <Skill key={i}>{skill}</Skill>)}
+          </SkillsContainer>
+        </StyledContent>
         <StyledPic>
-          <div className="wrapper">
-            <StaticImage
-              className="img"
-              src="../../images/me.jpg"
-              width={500}
-              quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF']}
-              alt="Headshot"
-            />
-          </div>
+          <StyledAvatarLink href={github}>
+            <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Avatar" />
+          </StyledAvatarLink>
         </StyledPic>
-      </div>
-    </StyledAboutSection>
+      </StyledFlexContainer>
+    </StyledContainer>
   );
+};
+
+About.propTypes = {
+  data: PropTypes.array.isRequired,
 };
 
 export default About;
